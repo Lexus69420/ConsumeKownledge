ef5ad5882e5728c9c62274472329e33bc81a4213
Lexus69420 <151642813+Lexus69420@users.noreply.github.com>
2024-03-17 13:55:33 +0000

    Update P17.py

--- P17.py
+++ P17.py
@@ -3,7 +3,42 @@ def count(ch):
     for i in range (len(ch)):
         sum+=1
     return sum
+all=[]
+twentyto100=[]
+sizes=["one","two","three","four","five","six","seven","eight","nine","ten","Eleven", "Twelve" , "Thirteen","Fourteen","Fifteen","Sixteen", "Seventeen","Eighteen","Nineteen" ]
+sizes2=["twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
+hundreds=[]
+hundreds2=[]
+hundreds3=[]
+hundreds4=[]
+hundreds5=[]
+
+for i in range(8):
+    for j in range(9):
+        twentyto100.append(sizes2[i]+sizes[j])
+        
+
+for i in range(9):
+    hundreds.append(sizes[i]+"hundred")
+    
+
+for i in range(9):
+    for j in range(len(sizes)):
+        hundreds2.append(hundreds[i]+"and"+sizes[j])
+
+for i in range(9):
+    for j in range(len(sizes2)):
+        hundreds3.append(hundreds[i]+"and"+sizes2[j])
+
+for i in range(8,9):
+    for j in range (len(twentyto100)):
+        hundreds5.append(hundreds[i]+"and"+twentyto100[j])
 
 
-sizes=["one","two","three","four","five","six","seven","eight","nine","ten","Eleven", "Twelve" , "Thirteen","Fourteen","Fifteen","Sixteen", "Seventeen","Eighteen","Nineteen","twenty" ]
+for i in range(8):
+    for j in range (len(twentyto100)):
+        hundreds4.append(hundreds[i]+"and"+twentyto100[j])
 
+
+all=all+twentyto100+sizes+sizes2+hundreds+hundreds2+hundreds3+hundreds4+hundreds5
+print(all)
